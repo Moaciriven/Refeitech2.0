4845bb4b3301d1a19a9d11e6b3acda55f7914af7
Moaciriven <ganevesbriel@gmail.com>
2025-03-13 16:49:24 -0300

    first commit

---
+++ src/main/java/dev/zeim/Refeitech2/Refeitech2Application.java
@@ -0,0 +1,13 @@
+package dev.zeim.Refeitech2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Refeitech2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Refeitech2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zeim/Refeitech2/Refeitech2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zeim.Refeitech2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Refeitech2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
